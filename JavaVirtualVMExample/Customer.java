33e8e8fd22151215676dfa7630861f5ce1964189
Yavdhesh <yavdheshsnchhr@gmail.com>
2026-08-10 10:10:40 -0400

    Create Customer.java

---
+++ JavaVirtualVMExample/Customer.java
@@ -0,0 +1,18 @@
+package com.garbagecollection.demo;
+
+public class Customer {
+
+    String name;
+    int age;
+
+    public Customer(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
