305310f5fb956e9efce6db7308a020d0aa0b999f
Yavdhesh <yavdheshsnchhr@gmail.com>
2026-08-10 10:10:12 -0400

    Create GarbageCollectionDemo.java

---
+++ JavaVirtualVMExample/GarbageCollectionDemo.java
@@ -0,0 +1,35 @@
+package com.garbagecollection.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+public class GarbageCollectionDemo {
+
+// This class when running should be viewed in VisualVM to see frequent Full GC running and young GC running.
+
+    public static void main(String [] args){
+        //-XX:MaxHeapSize=6m will trigger Full Pause eventually in VisualVM
+        RandomGenerator rd = new Random();
+        List<Customer> customers = new ArrayList<>();
+
+        while(true){
+
+            Customer s = null;
+            s = new Customer("Radhey"+rd.nextInt(10,10000), rd.nextInt(0,127));
+            customers.add(s);
+            if(customers.size()>10000){
+                for (int i = 0; i < 1000; i++) {
+                    customers.remove(0);
+                }
+            }
+
+
+
+
+        }
+
+
+    }
+}
